bb5b6d57b3de3097f140aa32c78b8917a48a96f6
Lucas Miles <lucasmiles@Lucass-MacBook-Pro.local>
2023-05-31 21:13:38 +1000

    addding imgs with else if and image path

--- weatherapp/src/Home.js
+++ weatherapp/src/Home.js
@@ -11,35 +11,41 @@ function Home() {
     humidity: 10,
     speed: 2
   })
-
-    useEffect(()=> {
-
-      const apiUrl ='https://api.openweathermap.org/data/2.5/weather?q=London&appid=94571ad5bfa119c8ca0026d8d2e72434&units=metric';
-      axios.get(apiUrl)
-      .then(res => console.log(res))
-      .catch( err => console.log(err));
+    const[name, setName] = useState('');
 
 
-    },[])
+const handleClick =() => {
+  if(name !== "") {
+    const apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=94571ad5bfa119c8ca0026d8d2e72434&units=metric`;
+    axios.get(apiUrl)
+    .then(res => {
+      console.log(res.data);
+      setData({...data, celcius: res.data.main.temp, name: res.data.name, humidity: res.data.main.humidity, speed: res.data.wind.speed})
+
+    })
+    .catch( err => console.log(err));
+  }
+
+}
 
   return (
     <div className="container">
       <div className=" weather">
         <div className=" search">
-          <input type= "text" placeholder='Enter City Name'/>
-          <button><img src="/Images/amazon/search.png" alt=" "/></button>
+          <input type= "text" placeholder='Enter City Name'onChange={e => setName(e.target.value)}/>
+          <button><img src="/Images/amazon/search.png" onClick={handleClick} alt=" "/></button>
         </div>
         <div className="winfo">
           <img src="/Images/amazon/clouds.png" alt=""/>
-          <h1>22°c</h1>
-          <h2>London</h2>
+          <h1>{Math.round(data.celcius)}°c</h1>
+          <h2>{data.name}</h2>
 
           <div className="details">
 
             <div className="col">
               <img src="/Images/amazon/humidity.png"></img>
               <div className="humidity">
-                <p>20%</p>
+                <p>{Math.round(data.humidity)}%</p>
                 <p>humidity</p>
               </div>
             </div>
@@ -48,7 +54,7 @@ function Home() {
             
               <img src="/Images/amazon/wind.png"></img>
               <div className="wind">
-                <p>2 km/h</p>
+                <p>{Math.round(data.speed)}</p>
                 <p>Wind</p>
               </div>
             </div>
