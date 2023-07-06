568650efc5fc1f11ee0d0d382fae4ea9a54d5576
Lucas Miles <lucasmiles@Lucass-MacBook-Pro.local>
2023-07-06 16:48:47 +1000

    Adding Game tictactoe.js for routing - also styling and place class name inside of tictactoe.js

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import Home from './Home'
-import TodoList from './TodoList'
+import TodoList from './Todolist'
+import TicTacToe from './TicTacToe'
 
 
 function App() {
   return (
 <div>
-
-    
     <Home />
    <TodoList/>
+   <TicTacToe/>
 
 </div>
   )
