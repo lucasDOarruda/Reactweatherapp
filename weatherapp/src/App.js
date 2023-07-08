6c80e231cdd0677b8b6aeabefd3e646c34bed972
Lucas Miles <lucasmiles@Lucass-MacBook-Pro.local>
2023-07-08 17:40:46 +1000

    adding TopMenuBar.jsx with name of the app And 4 menu

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -3,12 +3,14 @@ import Home from './Home'
 import TodoList from './components/Todolist'
 import TicTacToe from './components/TicTacToe'
 import Weather from './components/Weather'
+import TopMenuBar from './components/TopMenuBar'
 
 
 
 function App() {
   return (
 <div>
+  <TopMenuBar/>
     <Home />
    <TodoList/>
    <TicTacToe/>
---
+++ weatherapp/src/components/TopMenuBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../style.css'
+
+const TopMenuBar = () => {
+  return (
+
+    <div className="menu-bar">
+      <h1>Widget Garage</h1>
+      <div className="menu-item">Menu 1</div>
+      <div className="menu-item">Menu 2</div>
+      <div className="menu-item">Menu 3</div>
+      <div className="menu-item">Menu 4</div>
+    </div>
+  );
+}
+
+export default TopMenuBar;
