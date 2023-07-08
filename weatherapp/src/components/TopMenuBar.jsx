40cf3ed3019f6b12e88e46bad970c00a7f47d193
Lucas Miles <lucasmiles@Lucass-MacBook-Pro.local>
2023-07-08 18:53:59 +1000

    adding extra div and class for title

--- weatherapp/src/components/TopMenuBar.jsx
+++ weatherapp/src/components/TopMenuBar.jsx
@@ -3,14 +3,19 @@ import '../style.css'
 
 const TopMenuBar = () => {
   return (
+<div className='appname'>
+<h1 className='mytitle'>Widget Garage</h1>
 
     <div className="menu-bar">
-      <h1 className='mytitle'>Widget Garage</h1>
+      
+      <div className="menu-item">Home</div>
       <div className="menu-item">Weather</div>
       <div className="menu-item">Tic-Tac-Toe</div>
       <div className="menu-item">Todo List </div>
       <div className="menu-item">Contact</div>
     </div>
+</div>
+
   );
 }
 
